5421d10855d7a459a3e5432c8e6c092262a01f66
Tati Perevozniuk <fidget16322@gmail.com>
2022-11-15 22:31:44 +0200

    Add test case for login page

---
+++ pages/login_to_the_system.py
@@ -0,0 +1,32 @@
+import os
+import time
+import unittest
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium import webdriver
+from page.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestLoginPage(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver_service = Service(executable_path=DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=self.driver_service)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_log_in_to_the_system(self):
+        user_login = LoginPage(self.driver)
+        user_login.type_in_email('user01@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        time.sleep(4)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
